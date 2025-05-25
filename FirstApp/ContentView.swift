fb9b6566ad87634914a7f21b25b8c3d1da603f68
NicolNonga <nicodemosarmindo@gmail.com>
2025-05-25 22:32:23 +0100

    finish the send part of the lessson

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -4,23 +4,75 @@
 //
 //  Created by apple on 5/23/25.
 //
-
+// implicity return when u have one line of code
 
 import SwiftUI
 
 struct ContentView: View {
-    let  emojis:[String] = ["👻","🕷️","😈", "😈"]
+    let  emojis:[String] = ["👻","🕷️","😈", "😈", "👻","🕷️",  "😈", "👻", "🕷️","😈",  "👻","🕷️",  "😈", "👻", "🕷️","😈"
+                            , "👻","🕷️" ,"🕷️","😈",
+                            "😈", "👻", "🕷️","😈",  "👻","🕷️",
+    ]
+    @State var cardCount: Int = 3
     var body: some View {
         
-        HStack{
-            
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
-                
+        VStack {
+            ScrollView{
+                cards
             }
        
+            
+            Spacer()
+            
+            cardCountAjusters
+
+        }.padding()
+        
+
+
+    }
+    
+    var cards: some View {
+        
+        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]){
+            
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(contentMode: .fit)
+                
+            }
         }.foregroundColor(.orange)
             .padding()
+    }
+    
+    
+    var cardCountAjusters : some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        }     .imageScale(.large)
+            .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        
+        Button(action:{
+             cardCount += offset
+        } , label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 && offset < 0 || cardCount + offset > emojis.count)
+    }
+    
+    
+    var cardRemover : some View {
+        cardCountAdjuster(by: -1, symbol: "minus.circle.fill")
+    }
+
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "plus.circle.fill")
     }
     struct CardView: View {
         let content: String
@@ -32,13 +84,14 @@ struct ContentView: View {
                 //we use let here because roudedRectagle dont change
                 // we dont neew to tell the type of base cause swift know the type already
                 let  base  = RoundedRectangle(cornerRadius: 12)
-                if isFaceUp {
+                
+                 
+                Group {
                     base.fill(.white)
                     base.strokeBorder(lineWidth: 2)
                     Text(content).font(.largeTitle)
-                }else {
-                    base.fill()
-                }
+                }.opacity(isFaceUp ? 1 : 0)
+                base.fill().opacity(isFaceUp ? 0 : 1)
             }.onTapGesture {
                 //onTaGesture is just a function
                 isFaceUp.toggle() // change the value
